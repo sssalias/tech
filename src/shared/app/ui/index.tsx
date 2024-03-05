2f3af00c28277731378c69b338b498d049edc2a4
yukky9 <kozhurinayulia@gmail.com>
2024-03-05 20:35:27 +0300

    .

--- src/shared/app/ui/index.tsx
+++ src/shared/app/ui/index.tsx
@@ -1,7 +1,6 @@
 import DefaultButton from "./kit/Buttons/DefaultButton";
 import BorderButton from "./kit/Buttons/BorderButton";
 import Logo from "./kit/Logo/Logo";
-import Avatar from "./kit/Profile/Avatar";
 import BaseLayout from "./layouts/BaseLayout/BaseLayout";
 import Header from "./layouts/Header/Header";
 
@@ -9,7 +8,6 @@ export  {
     DefaultButton,
     BorderButton,
     Logo,
-    Avatar,
     BaseLayout,
     Header
 }
