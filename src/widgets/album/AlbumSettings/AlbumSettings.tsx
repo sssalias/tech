222a609e0baf162cbdf206f16f373b3b75156187
yukky9 <kozhurinayulia@gmail.com>
2024-03-09 20:41:35 +0300

    checkbox

--- src/widgets/album/AlbumSettings/AlbumSettings.tsx
+++ src/widgets/album/AlbumSettings/AlbumSettings.tsx
@@ -6,12 +6,16 @@ import React from 'react'
 //      2) - Сделать уровени доступа (owners)
 
 const AlbumSettings = () => {
+    const [isChecked, setChecked] = React.useState();
     return (
         <div className='w-1/6 ml-auto border-l-2 border-gray-200 b'>
             <div className='w-full flex flex-col justify-center items-center'>
                 <h3 className='text-lg font-semibold'>Настройки альбома</h3>
                 <div>
-
+                    <label className="flex gap-2">
+                        <input type="checkbox"/>
+                        Сделать приватным
+                    </label>
                 </div>
             </div>
         </div>
